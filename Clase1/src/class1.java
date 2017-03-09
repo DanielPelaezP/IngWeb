49f198b9d5f78d34d559f81aa6a2f5fb6e54d838
daniel.pelaezp <daniel.pelaezp@lis-sala4>
2017-03-09 12:32:52 -0500

    aqui hice un commit :3, BD

---
+++ Clase1/src/class1.java
@@ -0,0 +1,46 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+public class class1 {
+	
+	public void consultarCiudades(){
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		try{
+			Class.forName("com.mysql.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/BDclase1","root","root");//conecxion con base de datos
+			ps = con.prepareStatement("select * From ciudades");
+			rs = ps.executeQuery();
+			while(rs.next()){
+				System.out.println(rs.getString("codigo") + ": " + rs.getString("nombre"));
+			}
+			}catch(ClassNotFoundException e){
+				e.printStackTrace();
+			}catch(SQLException e){
+				e.printStackTrace();
+				
+			}
+		finally{
+			try{
+			rs.close();
+			ps.close();
+			con.close();
+			}catch(SQLException e){
+				e.printStackTrace();
+			
+		}
+			}
+	}
+
+	public static void main(String[] args) {
+		
+	
+		
+	}
+
+}
